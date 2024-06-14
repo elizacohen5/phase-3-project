7e851825b60f834f80f1884aa3213fee6d4792b7
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-06-14 16:46:35 -0400

    create_student method

--- lib/cli.py
+++ lib/cli.py
@@ -1,27 +1,42 @@
 # lib/cli.py
 
 from helpers import (
-    exit_program,
-    helper_1
+    # exit_program,
+    helper_1,
+    create_student
 )
 
 
 def main():
-    while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            helper_1()
-        else:
-            print("Invalid choice")
+    # while True: #TODO: check if we need it
+        print("Welcome")
+        create_student()
+        
+    #     choice = input("> ")
+    #     if choice == "0":
+    #         exit_program()
+    #     elif choice == "1":
+    #         helper_1()
+    #     else:
+    #         print("Invalid choice")
 
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+# def menu():
+#     print("Welcome")
+    
+    
+#     print("Please select an option:")
+#     print("0. Exit the program")
+#     print(f"""
+# Welcome to the [adjective] world of the Flatiron School Coding Bootcamp, where every day feels like a [ridiculous event] networking event orchestrated by a [fictional villain] Targaryen ! Each morning starts at [time] 3pm, armed with [beverage] gin and tonic and a [emotion] depressed sense of determination.
+
+# You walk into class at [time] 7am sharp, clutching your [beverage] earl grey tea like it’s the Holy Grail. Your instructor, [celebrity’s name] Sydney Sweeney, is already at the board, scribbling furiously about [technical topic] Dictionaries. With the energy of a [hyperactive animal] Jaguar on [stimulant] speed, they explain how [programming concept] OOP is just like [getting a cat to fetch].
+
+# Lunch rolls around, and you dash out, daydreaming about [favorite comfort food]. You settle down with your [questionable meal] and a side of [emotional turmoil], while everyone else debates [the next big tech trend] like it’s [a reality TV show]. You try to relax, but your mind keeps wandering back to [unfinished project]. Why does it seem to [have a vendetta] against you?
+# Back to the grind. It’s time to tackle [Canvas], the [mysterious] platform that holds all your [assignments]. Navigating Canvas is like playing [a sadistic video game] where every level is harder than the last. You encounter the infamous [“page not loading”] error. Canvas continues to [taunt you] with its [error messages] and [cryptic instructions], but you bravely march on, solving problems like a [determined protagonist] in a [gritty action movie].
+# It’s time for coding practice, where you attempt to tame your [rebellious code]. Your project, “[The Impossible App],” is supposed to be a [simple] task, but it morphs into a [nightmare monster] with [bugs] the size of [fictional creatures]. You tackle the [errors] like a [wild-west gunslinger], but they keep [multiplying] like [tribbles in Star Trek]. By [late evening], you’ve written [number] lines of code and deleted [larger number].
+# The day ends, and you stagger out like a [zombie] from a [horror movie]. Tomorrow, you’ll battle [React], but tonight you just want to [curl up] with [favorite comfort item] and [forget the world].
+# """)
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,20 @@
 # lib/helpers.py
+from models.student import Student
 
 def helper_1():
     print("Performing useful function#1.")
 
-
+#TODO Once we finished with story
 def exit_program():
-    print("Goodbye!")
+    print("Thank you for your story!")
     exit()
+
+def create_student():
+    name = input("Please, enter your name: ")
+    phase = int(input("Please, enter your phase:"))
+    program = input("Please, enter your program:")
+    try:
+        student = Student.create(name, phase, program)
+        print(f"Success: {student}")
+    except Exception as exc:
+        print("Error", exc)
