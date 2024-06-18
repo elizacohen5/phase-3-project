c4fa84dc4707f7d4bc569622d5c165d3d064e818
Angela Tolwani <angela@tolwani.com>
2024-06-18 14:19:01 -0400

    cleanup

--- lib/seed.py
+++ lib/seed.py
@@ -1,6 +1,6 @@
 from models import CONN, CURSOR
 from models.student import Student
-from models.story import Story 
+from models.story import Story
 
 def seed_database():
     Student.drop_table()
@@ -9,13 +9,16 @@ def seed_database():
     Story.create_table()
 
     # Create seed data
-    s1 = Student.create("Eliza", 3, "SE")
-    # s1.save()
-    s2 = Student.create("Alina", 3, "SE")
-    # s2.save()
-    s3 = Student.create("Angela", 3, "SE")
+    s1 = Student.create("eliza", 3, "SE")
+    s2 = Student.create("alina", 3, "SE")
+    s3 = Student.create("angela", 3, "SE")
 
-    story = Story.create(s1, "hi")
+    story1 = Story.create(s1.id, s1.name, """Welcome to the happy world of the Flatiron School Coding Bootcamp, where every day feels like a networking event orchestrated by 
+Targaryen!""")
+    story2 = Story.create(s2.id, s2.name,"""Welcome to the sad world of the Flatiron School Coding Bootcamp, where every day feels like a sleepover orchestrated by 
+The White Walker!""")
+    story3 = Story.create(s3.id, s3.name, """Welcome to the exciting world of the Flatiron School Coding Bootcamp, where every day feels like a marathon orchestrated by 
+Lannister!""")
 
 
 seed_database()
